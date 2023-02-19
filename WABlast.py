3daac802b4f1e30717dc5f72ed5e9a0ae1b707df
Mochamad Rivandy Kesuma Nugraha <111955620+rivandykesuma@users.noreply.github.com>
2023-02-19 08:52:25 +0700

    Add files via upload

---
+++ WABlast.py
@@ -0,0 +1,27 @@
+import pywhatkit as pwt
+import pyautogui
+import time
+
+
+
+caption = """
+
+caption in here 
+
+"""
+
+ 
+#mode safe --> 1 Per minute
+number = ['+628xxxxxxxxxx']
+for i in range(len(number)):
+    try:
+        pwt.sendwhats_image(number[i], 'image.png',caption=caption, wait_time=30)
+        time.sleep(7)
+        pyautogui.click(x=1292, y=806)
+        pyautogui.press('enter')
+        time.sleep(7)
+        pyautogui.hotkey('ctrl','w')
+        print(i)
+    except:
+        print('eror')
+        print(i)
